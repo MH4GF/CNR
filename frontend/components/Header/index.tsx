3ba503e2262895c2a11d72f8da560a9563fa9959
MH4GF <rixi436@gmail.com>
2020-09-22 18:34:02 +0900

    fixed header position

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -4,6 +4,9 @@ import styled from "styled-components";
 
 const Container = styled.div`
   padding: 2.5rem 0;
+  position: sticky;
+  top: 0;
+  z-index: 100;
 `;
 
 export const Header: React.FC = () => {
