593559f6ba5d5b2e1f34b8f22640c011b47de7d3
MH4GF <rixi436@gmail.com>
2020-09-26 18:43:08 +0900

    lang=jaを追加

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
